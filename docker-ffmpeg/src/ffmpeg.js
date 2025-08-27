d97981cc0aa714d217caf6a2fe58a0b01cd79daa
Brendon O'Neill <brendonpjon@gmail.com>
2025-08-27 17:15:35 +0100

    Working at a basic level
    users can send videos over to the second server it gets put in a queue and starts ffmpeg child process. when task is complete it sends back the video file. It is patched together but I just wanted to see it work

--- docker-ffmpeg/src/ffmpeg.js
+++ docker-ffmpeg/src/ffmpeg.js
@@ -1,18 +1,48 @@
 import Express from 'express'
 import upload from '../utils/multer.js'
 import fs from "node:fs"
+import { main } from '../utils/ffmpeg.js'
+import cors from 'cors';
 
 const app = Express()
+// Built-in JSON parser
+app.use(Express.json());
 
+// Built-in URL-encoded parser
+app.use(Express.urlencoded({ extended: true }));
 
+app.use('/store-video', Express.raw({ 
+  type: 'video/*', 
+  limit: '5gb' // Adjust based on your video size limits
+}));
 
-app.use("/test", upload.single("file"), (req, res) => {
- console.log(req.file)
- const writeStream = fs.createWriteStream("./main.mkv");
- req.pipe(writeStream)
- res.json("hello myself")
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['*']
+}));
+
+app.use("/hello", (req, res) => {
+ console.log("test link")
+ res.json("success")
 })
 
-app.listen("3000",() => {
-    console.log(`server running on 3005`)
+app.use("/", (req, res) => {
+ console.log("i'm being called")
+ const writeStream = fs.createWriteStream(`temp/${req.headers['x-original-filename']}`);
+ req.pipe(writeStream)
+ writeStream.on("finish", () => {
+  main(req.headers['x-original-filename'])
+  res.json("success")
+ })
+ writeStream.on("error", () => {
+  res.json("error")
+ })
+ 
+})
+
+
+app.listen("3007",() => {
+    console.log(`server running on 3007`)
 })
---
+++ docker-ffmpeg/utils/ffmpeg.js
@@ -0,0 +1,80 @@
+import { spawnSync } from 'child_process';
+import { EventEmitter } from 'node:events';
+import fs from "node:fs"
+const videoEmitter = new EventEmitter();
+const videoList = []
+let active = false
+
+videoEmitter.on('add', videoAddTask);
+videoEmitter.on('finished', returnVideo)
+
+export async  function main(fileName)
+{
+ console.log("Main is called")
+ videoEmitter.emit('add', fileName)
+}
+
+export function videoAddTask(fileName)
+{
+  console.log("videoAddTask")
+  videoList.push(fileName)
+  if(!active)
+  {
+    active = true
+    videoWork()
+    active = false
+  }
+  return
+}
+
+
+
+
+function videoWork()
+{
+    while(videoList.length >= 1)
+    {
+        console.log("test was called")
+        test(videoList[0])
+        videoEmitter.emit('finished', `new${videoList[0]}`)
+        videoList.shift()
+    }
+    return 
+}
+
+
+function test(file)
+{
+    console.log(videoList[0], ' is getting converted')
+    const results = spawnSync('ffmpeg',['-i', `temp/${file}`, '-map', '0:0', '-map', '0:1', '-c', 'copy', `temp/new${file}`],{})
+
+    if (results.error) {
+        throw new Error(`FFmpeg spawn error: ${result.error.message}`);
+    }
+
+    if (results.status !== 0) {
+    throw new Error(
+      `FFmpeg exited with code ${results.status}: ${results.stderr}`
+    );
+    }
+
+    console.log(videoList[0], ' completed')
+}
+
+
+
+async function returnVideo(videoFile)
+{
+      const readStream = fs.createReadStream(`temp/${videoFile}`);
+      const stats = fs.statSync(`temp/${videoFile}`);
+      console.log("Called-----")
+      try {
+          const a = await fetch("http://localhost:3000/videoreturn", {method:"POST", body:readStream, headers: {
+          'Content-Type': 'video/mkv',
+          'Content-Length': stats.size.toString(),
+          'X-Original-Filename': videoFile
+        },duplex: 'half'})
+      } catch (error) {
+          console.log(error)
+      }
+}
